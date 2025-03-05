9cad3e32d2905f30f294b6f2a29c3581d87e27a4
Hvnttr G <rhgarguer@gmail.com>
2025-03-05 19:17:51 +0100

    host 3

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ initializeCronJobs();
 const corsOptions = {
   origin:'*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 };
 
 app.use(cors(corsOptions));
